33bd51a8265dcf1c78fbc5cb10021ca223d21c90
Maurice Diesendruck <moglobal@gmail.com>
2017-10-05 00:02:23 -0500

    adapted eval_samples_thin.py to fetch sample files from results//logs/

--- eval_samples_thin.py
+++ eval_samples_thin.py
@@ -1,3 +1,4 @@
+import argparse
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -5,12 +6,19 @@ plt.style.use('ggplot')
 import numpy as np
 import pdb
 import pylab
+import os
 from scipy.stats import shapiro, probplot, norm
 
 
-x_orig = np.load('sample_x.npy')
-g_orig = np.load('sample_g.npy')
-z_orig = np.load('sample_z.npy')
+parser = argparse.ArgumentParser()
+parser.add_argument('--expt', type=str, default='test')
+args = parser.parse_args()
+expt = args.expt
+base_path = os.path.join('results', expt, 'logs')
+
+x_orig = np.load(os.path.join(base_path, 'sample_x.npy'))
+g_orig = np.load(os.path.join(base_path, 'sample_g.npy'))
+z_orig = np.load(os.path.join(base_path, 'sample_z.npy'))
 
 x = x_orig.reshape(x_orig.shape[0],)
 g = g_orig.reshape(g_orig.shape[0],)
@@ -27,25 +35,29 @@ z_sorted = z[z_sort_order]
 g_sorted_by_z = g[z_sort_order]
 
 # Shapiro-Wilk test for normality of generated samples.
-#(w_statistic, p_value) = shapiro(g)
-(w_statistic, p_value) = 0, 0
+(w_statistic, p_value) = shapiro(g)
 
 # Plot results.
-plt.figure(figsize=(25, 10))
+plt.figure(figsize=(20, 15))
 plt.suptitle('Comparison: X, G, Z. ShapiroWilk(w,p)=({:.04f},{:.04f})'.format(
     w_statistic, p_value), fontsize=20)
 
-plt.subplot(211)
+ax1 = plt.subplot(2, 1, 1)
 plt.hist(x, 30, normed=True, color='green', label='x', alpha=0.3)
 plt.hist(g, 30, normed=True, color='blue', label='g', alpha=0.3)
 xs = np.linspace(min(x), max(x), 100)
-ys = norm.pdf(xs, 0, 1)
-plt.plot(xs, ys, color='red', label='pdf', alpha=0.3)
-plt.title('Distribution comparison: X vs G')
+ys1 = norm.pdf(xs, 0, 0.5)
+ys2 = norm.pdf(xs, 2, 0.5)
+ys = 2. / 3. * ys1 + 1. / 3. * ys2
+ys_unthinned = 0.5 * ys1 + 0.5 * ys2
+plt.plot(xs, ys, color='green', label='pdf', alpha=0.7)
+plt.plot(xs, ys_unthinned, color='black',
+         label='pdf_unthinned', alpha=0.3)
+plt.title('Distribution comparison: X vs G, m={}, n={}'.format(len(x), len(g)))
 plt.xlabel('Values')
 plt.legend()
 
-plt.subplot(245)
+plt.subplot(2, 4, 5)
 plt.plot(np.sort(g), color='blue', label='g_sorted', alpha=0.3)
 plt.plot(np.sort(x), color='green', label='x_sorted', alpha=0.3)
 plt.title('Ordered comparison: X vs G')
@@ -53,24 +65,22 @@ plt.xlabel('Samples')
 plt.ylabel('Values g, x')
 plt.legend()
 
-plt.subplot(246)
+plt.subplot(2, 4, 6)
 probplot(g, dist='norm', plot=pylab)
 plt.title('QQ Plot: G')
 
-plt.subplot(247)
+plt.subplot(2, 4, 7)
 probplot(x, dist='norm', plot=pylab)
 plt.title('QQ Plot: X')
 
-plt.subplot(248)
-#plt.title('Mapping from noise z to g')
-#plt.xlabel('Samples')
-#plt.ylabel('Values z, g')
-#plt.plot(z_sorted, color='green', label='z_sorted', alpha=0.3)
-#plt.plot(g_sorted_by_z, color='blue', label='g_sorted_by_z', alpha=0.3)
-#plt.legend()
+plt.subplot(2, 4, 8)
 plt.scatter(z, g)
 plt.title('Mapping from noise z to g')
 plt.xlabel('Noise z')
 plt.ylabel('Generated value')
 
-plt.savefig('result_plot.png')
+filename = os.path.join(base_path, 'result_plot.png')
+plt.savefig(filename)
+
+os.system(('echo $PWD | mutt momod@utexas.edu -s "1dgan result_plot"'
+           ' -a "{}"').format(filename))
