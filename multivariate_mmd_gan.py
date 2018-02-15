a8f15d9a9cf8434a56e7ae13220e9c1c4c4c8a77
diesendruck <moglobal@gmail.com>
2018-02-15 00:45:00 -0600

    improved scatterplots; added minibatch

--- multivariate_mmd_gan.py
+++ multivariate_mmd_gan.py
@@ -12,21 +12,23 @@ layers = tf.layers
 
 # Config.
 parser = argparse.ArgumentParser()
-parser.add_argument('--data_num', type=int, default=100)
-parser.add_argument('--z_dim', type=int, default=10)
+parser.add_argument('--data_num', type=int, default=1000)
+parser.add_argument('--batch_size', type=int, default=100)
+parser.add_argument('--z_dim', type=int, default=2)
 parser.add_argument('--width', type=int, default=5,
                     help='width of generator layers')
 parser.add_argument('--depth', type=int, default=5,
                     help='num of generator layers')
-parser.add_argument('--log_step', type=int, default=500)
+parser.add_argument('--log_step', type=int, default=2500)
 parser.add_argument('--max_step', type=int, default=200000)
-parser.add_argument('--learning_rate', type=float, default=1e-4)
-parser.add_argument('--optimizer', type=str, default='adam',
+parser.add_argument('--learning_rate', type=float, default=1e-3)
+parser.add_argument('--optimizer', type=str, default='rmsprop',
                     choices=['adagrad', 'adam', 'gradientdescent', 'rmsprop'])
 parser.add_argument('--data_file', type=str, default=None)
 
 args = parser.parse_args()
 data_num = args.data_num
+batch_size = args.batch_size
 z_dim = args.z_dim
 width = args.width
 depth = args.depth
@@ -36,8 +38,6 @@ learning_rate = args.learning_rate
 optimizer = args.optimizer
 data_file = args.data_file
 activation = tf.nn.elu
-save_tag = 'dn{}_zd{}_w{}_d{}_lr{}_op_{}'.format(
-    data_num, z_dim, width, depth, learning_rate, optimizer)
 
 # Load data.
 if data_file:
@@ -49,10 +49,16 @@ if data_file:
 else:
     n1 = data_num / 2
     n2 = data_num - n1
-    cluster1 = np.random.multivariate_normal([0., 0.], [[1., 0.], [0., 1.]], n1)
-    cluster2 = np.random.multivariate_normal([6., 6.], [[1., 0.], [0., 1.]], n2)
+    cluster1 = np.random.multivariate_normal(
+        [-2., 5.], [[1., .9], [.9, 1.]], n1)
+    cluster2 = np.random.multivariate_normal(
+        [6., 6.], [[1., 0.], [0., 1.]], n2)
     data = np.concatenate((cluster1, cluster2))
     out_dim = data.shape[1]
+
+# Set save tag, as a function of config parameters.
+save_tag = 'dn{}_zd{}_w{}_d{}_lr{}_op_{}'.format(
+    data_num, z_dim, width, depth, learning_rate, optimizer)
 
 # Set up log dir.
 log_dir = 'logs'
@@ -80,8 +86,8 @@ def generator(z, width=3, depth=3, activation=tf.nn.elu, out_dim=1,
 
 
 # Build model.
-x = tf.placeholder(tf.float64, [data_num, out_dim], name='x')
-z = tf.placeholder(tf.float64, [data_num, z_dim], name='z')
+x = tf.placeholder(tf.float64, [batch_size, out_dim], name='x')
+z = tf.placeholder(tf.float64, [batch_size, z_dim], name='z')
 g = generator(z, width=width, depth=depth, activation=activation,
               out_dim=out_dim)
 v = tf.concat([x, g], 0)
@@ -89,18 +95,20 @@ VVT = tf.matmul(v, tf.transpose(v))
 sqs = tf.reshape(tf.diag_part(VVT), [-1, 1])
 sqs_tiled_horiz = tf.tile(sqs, tf.transpose(sqs).get_shape())
 exp_object = sqs_tiled_horiz - 2 * VVT + tf.transpose(sqs_tiled_horiz)
-sigma = 1
-K = tf.exp(-0.5 * (1 / sigma) * exp_object)
-# TODO: Add sum of kernels.
-K_xx = K[:data_num, :data_num]
-K_yy = K[data_num:, data_num:]
-K_xy = K[:data_num, data_num:]
+K = 0.0
+sigma_list = [0.1, 1., 2., 8.]
+for sigma in sigma_list:
+    gamma = 1.0 / (2 * sigma**2)
+    K += tf.exp(-gamma * exp_object)
+K_xx = K[:batch_size, :batch_size]
+K_yy = K[batch_size:, batch_size:]
+K_xy = K[:batch_size, batch_size:]
 K_xx_upper = tf.matrix_band_part(K_xx, 0, -1)
 K_yy_upper = tf.matrix_band_part(K_yy, 0, -1)
-num_combos = data_num * (data_num - 1) / 2
+num_combos = batch_size * (batch_size - 1) / 2
 mmd = (tf.reduce_sum(K_xx_upper) / num_combos +
        tf.reduce_sum(K_yy_upper) / num_combos -
-       2 * tf.reduce_sum(K_xy) / (data_num * data_num))
+       2 * tf.reduce_sum(K_xy) / (batch_size * batch_size))
 g_vars = [var for var in tf.global_variables() if 'generator' in var.name]
 if optimizer == 'adagrad':
     opt = tf.train.AdagradOptimizer
@@ -119,23 +127,24 @@ sess.run(init_op)
 print args
 start_time = time()
 for i in range(max_step):
+    random_batch_data = np.array(
+        [data[d] for d in np.random.choice(len(data), batch_size)])
+    random_batch_z = get_random_z(batch_size, z_dim)
     sess.run(g_optim,
              feed_dict={
-                 z: get_random_z(data_num, z_dim),
-                 # x: np.random.choice(data, (data_num, 1))})
-                 x: data})
+                 z: random_batch_z,
+                 x: random_batch_data})
 
     # Occasionally log/plot results.
     if i % log_step == 0:
         mmd_out, g_out = sess.run(
             [mmd, g], feed_dict={
-                z: get_random_z(data_num, z_dim),
-                # x: np.random.choice(data, (data_num, 1))})
-                x: data})
+                z: random_batch_z,
+                x: random_batch_data})
         print '\niter:{} mmd = {}'.format(i, mmd_out)
         fig, ax = plt.subplots()
-        ax.scatter(*zip(*g_out), color='blue', alpha=0.3)
-        ax.scatter(*zip(*data), color='green', alpha=0.3)
+        ax.scatter(*zip(*data), color='gray', alpha=0.05)
+        ax.scatter(*zip(*g_out), color='green', alpha=0.3)
         plt.savefig(os.path.join(
             log_dir, 'scatter_{}_i{}.png'.format(save_tag, i)))
         plt.close(fig)
@@ -147,6 +156,6 @@ for i in range(max_step):
             m, s = divmod(total_est, 60)
             h, m = divmod(m, 60)
             total_est_str = '{:.0f}:{:02.0f}:{:02.0f}'.format(h, m, s)
-            print ('\nTime (s). Elapsed: {:.2f}, Avg/iter: {:.4f},'
+            print ('\nTime (s). Elapsed: {:.2f}, time/iter: {:.4f},'
                    ' Total est.: {}').format(elapsed_time, time_per_iter,
                                              total_est_str)
