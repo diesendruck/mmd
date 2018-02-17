7b7ff3b0bbe2bbb01d29e40bbfbe027d21b8f41e
Maurice Diesendruck <moglobal@gmail.com>
2018-02-16 18:13:26 -0600

    added plot of data, support points, and proper subset; added weighting helper function; added subset.npy, which contains the snapped subset of the data; added weights.npy, which contains weights for each data point as a function of distance to the nearest neighbor in subset.npy

--- weighting.py
+++ weighting.py
@@ -7,28 +7,43 @@ import pdb
 from numpy.linalg import norm
 
 
+def nearest(target, arr):
+    nearest_dist = 1e10
+    nearest_neighbor = [0, 0]
+    for candidate in arr:
+        dist = norm(target - candidate)
+        if dist < nearest_dist:
+            nearest_dist = dist
+            nearest_neighbor = candidate 
+    return nearest_dist, np.array(nearest_neighbor)
+
+
+# Load data and support points.
 data = np.loadtxt(open('gp_data.txt', 'rb'), delimiter=' ')
 support_points = np.load('g_out.npy')
 
 assert data.shape[1] == support_points.shape[1], \
     'dimension of data and support points.g must match'
 
-proper_subset = []
+# Get datapoints (the subset) closest to support points.
+subset = []
 for sp in support_points:
-    nearest_dist = 1e10
-    nearest_neighbor = [0, 0]
-    for dp in data:
-        dist = norm(sp - dp)
-        if dist < nearest_dist:
-            nearest_dist = dist
-            nearest_neighbor = dp
-    proper_subset.append(nearest_neighbor)
+    _, nearest_neighbor = nearest(sp, data)
+    subset.append(nearest_neighbor)
+np.save('subset.npy', np.array(subset))
 
+# Plot data, support points, and subset.
 fig, ax = plt.subplots()
 ax.scatter(*zip(*data), color='gray', alpha=0.05, label='data')
 ax.scatter(*zip(*support_points), color='green', alpha=0.3, label='support')
-ax.scatter(*zip(*proper_subset), color='red', alpha=0.3, label='subset')
+ax.scatter(*zip(*subset), color='red', alpha=0.3, label='subset')
 ax.legend()
-plt.savefig('plot_proper_subset.png')
-pdb.set_trace()
+plt.savefig('plot_data_support_subset.png')
 
+# Compute data weights.
+c = 1
+weights = np.zeros(data.shape[0])
+for index, dp in enumerate(data):
+    nearest_dist, _ = nearest(dp, subset) 
+    weights[index] = c * 1.0 / (nearest_dist + 1e-6)
+np.save('weights.npy', weights)
