99d2ebc017b6badc00883b2df1acf907e63558e1
Maurice Diesendruck <moglobal@gmail.com>
2018-03-06 14:04:28 -0600

    added correlation plots and trained model

--- simulation_studies/med/multivariate_mmd_gan.py
+++ simulation_studies/med/multivariate_mmd_gan.py
@@ -12,25 +12,32 @@ layers = tf.layers
 import pandas as pd
 import seaborn as sb
 from scipy.stats import scoreatpercentile
+from scipy.stats import pearsonr 
 
 
 # Config.
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_num', type=int, default=1000)
-parser.add_argument('--batch_size', type=int, default=500)
-parser.add_argument('--gen_num', type=int, default=200)
-parser.add_argument('--z_dim', type=int, default=16)
-parser.add_argument('--width', type=int, default=256,
-    help='width of generator layers')
+parser.add_argument('--batch_size', type=int, default=1000)
+parser.add_argument('--gen_num', type=int, default=1000)
+parser.add_argument('--z_dim', type=int, default=64)
+parser.add_argument('--width', type=int, default=100,
+                    help='width of generator layers')
 parser.add_argument('--depth', type=int, default=3, 
-    help='num of generator layers')
-parser.add_argument('--lambda_mmd', type=float, default=1e-4)
-parser.add_argument('--log_step', type=int, default=2000)
+                    help='num of generator layers')
+parser.add_argument('--lambda_mmd', type=float, default=1e-1)
+parser.add_argument('--log_step', type=int, default=500)
 parser.add_argument('--max_step', type=int, default=500000)
-parser.add_argument('--learning_rate', type=float, default=1e-4)
+parser.add_argument('--learning_rate', type=float, default=1e-3)
+parser.add_argument('--lr_update_step', type=int, default=20000)
 parser.add_argument('--optimizer', type=str, default='rmsprop',
-    choices=['adagrad', 'adam', 'gradientdescent', 'rmsprop'])
-parser.add_argument('--data_file', type=str, default=None)
+                    choices=['adagrad', 'adam', 'gradientdescent', 'rmsprop'])
+parser.add_argument('--data_file', type=str, default='data0.csv')
+parser.add_argument('--tag', type=str, default='test')
+parser.add_argument('--load_existing', action='store_true', default=False,
+                    dest='load_existing')
+parser.add_argument('--sample_n', type=int, default=0,
+                    help='sample n from existing model then exit')
 
 args = parser.parse_args()
 data_num = args.data_num
@@ -43,7 +50,11 @@ lambda_mmd = args.lambda_mmd
 log_step = args.log_step
 max_step = args.max_step
 learning_rate = args.learning_rate
+lr_update_step = args.lr_update_step
 optimizer = args.optimizer
+tag = args.tag
+load_existing = args.load_existing
+sample_n = args.sample_n
 data_file = args.data_file
 activation = tf.nn.elu
 
@@ -87,72 +98,6 @@ def build_toy_data():
     #cluster2 = np.random.multivariate_normal(
     #    [6., 6.], [[1., 0.], [0., 1.]], n2)
     #data = np.concatenate((cluster1, cluster2))
-
-
-def plot_summary_raw_data(raw_data):
-    num_cols = raw_data.shape[1]
-    sq_dim = int(np.ceil(np.sqrt(num_cols)))
-    fig, axs = plt.subplots(sq_dim, sq_dim, figsize=(10,10))
-    fig.subplots_adjust(hspace=0.5, wspace=0.5)
-    axs = axs.ravel()
-    for i in range(num_cols):
-        axs[i].hist(raw_data[:, i])
-    plt.savefig('plot_raw_data_marginals.png')
-    plt.close('all')
-
-
-# load_data().
-if data_file:
-    raw_data = np.loadtxt(open(data_file, 'rb'), delimiter=',')
-    num_cols = raw_data.shape[1]
-    plot_summary_raw_data(raw_data)
-    norm_data = 1
-
-    print('\nRaw data:')
-    for i in range(num_cols):
-        print('{: >17},{: >17},{: >17},{: >17},{: >17}'.format(
-            *fivenum(raw_data[:,i])))
-    if norm_data:
-        print('\nNormed data:')
-        # Don't normalize binary vars.
-        mean_mask = [1] * 17 + [0, 0]
-        std_mask = [1] * 19
-        mean_vec = raw_data.mean(0) * mean_mask
-        std_vec = raw_data.std(0) * std_mask
-        # Regularly normalize the rest.
-        #data = (raw_data - raw_data.mean(0)) / raw_data.std(0)
-        data = (raw_data - mean_vec) / std_vec 
-        for i in range(data.shape[1]):
-            print('{: >17},{: >17},{: >17},{: >17},{: >17}'.format(
-                *fivenum(data[:,i])))
-
-    # Optionally exclude some columns.
-    excluded_cols = []
-    cols_to_use = [c for c in range(num_cols) if c not in excluded_cols]
-    do_subset_data = 1
-    if do_subset_data:
-        data = data[:, cols_to_use]
-
-    data_num = len(data)
-    out_dim = data.shape[1]
-else:
-    data = build_toy_data()
-    data = (data - data.mean(0))/data.std(0)
-    cols_to_use = np.arange(data.shape[1])
-    out_dim = data.shape[1]
-
-# Set save tag, as a function of config parameters.
-save_tag = 'dn{}_bs{}_gen{}_zd{}_w{}_d{}_lr{}_op_{}'.format(
-    data_num, batch_size, gen_num, z_dim, width, depth, learning_rate,
-    optimizer)
-
-# Set up log dir.
-log_dir = 'logs'
-checkpoint_dir = 'logs/checkpoints'
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
-if not os.path.exists(checkpoint_dir):
-    os.makedirs(checkpoint_dir)
 
 
 def get_random_z(gen_num, z_dim):
@@ -279,12 +224,87 @@ def compute_mmd(enc_x, enc_g, use_tf=True):
         return mmd, exp_object
 
 
-# Build model.
-x = tf.placeholder(tf.float64, [batch_size, out_dim], name='x')
-z = tf.placeholder(tf.float64, [gen_num, z_dim], name='z')
+def load_checkpoint(saver, sess, checkpoint_dir):
+    import re
+    print(" [*] Reading checkpoints...")
+    print("     {}".format(checkpoint_dir))
+    ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+    if ckpt and ckpt.model_checkpoint_path:
+        ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
+        saver.restore(sess, os.path.join(checkpoint_dir, ckpt_name))
+        counter = int(ckpt_name.split('-')[-1])
+        print(" [*] Success to read {}".format(ckpt_name))
+        return True, counter
+    else:
+        print(" [*] Failed to find a checkpoint")
+        return False, 0
+
+
+def prepare_dirs():
+    """Set up log dir."""
+    log_dir = 'logs_{}'.format(tag)
+    checkpoint_dir = os.path.join(log_dir, 'checkpoints')
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+    return log_dir, checkpoint_dir
+
+
+# Prepare directories for logging and checkpoints.
+log_dir, checkpoint_dir = prepare_dirs()
+
+# load_data().
+if data_file:
+    raw_data = np.loadtxt(open(data_file, 'rb'), delimiter=',')
+    num_cols = raw_data.shape[1]
+    norm_data = 1
+
+    print('\nRaw data:')
+    for i in range(num_cols):
+        print('{: >17},{: >17},{: >17},{: >17},{: >17}'.format(
+            *fivenum(raw_data[:,i])))
+    if norm_data:
+        print('\nNormed data:')
+
+        # Don't normalize binary vars.
+        # NOTE: This is tailored to each data set!
+        mean_mask = [1] * 17 + [0, 0, 0]  
+        std_mask = [1] * 20
+
+        mean_vec = raw_data.mean(0) * mean_mask
+        std_vec = raw_data.std(0) * std_mask
+        data = (raw_data - mean_vec) / std_vec 
+
+        for i in range(data.shape[1]):
+            print('{: >17},{: >17},{: >17},{: >17},{: >17}'.format(
+                *fivenum(data[:,i])))
+
+    # Optionally exclude some columns.
+    excluded_cols = []
+    cols_to_use = [c for c in range(num_cols) if c not in excluded_cols]
+    do_subset_data = 1
+    if do_subset_data:
+        data = data[:, cols_to_use]
+
+    data_num = len(data)
+    out_dim = data.shape[1]
+else:
+    data = build_toy_data()
+    data = (data - data.mean(0))/data.std(0)
+    cols_to_use = np.arange(data.shape[1])
+    out_dim = data.shape[1]
+
+
+# build_model().
+x = tf.placeholder(tf.float64, [None, out_dim], name='x')
+z = tf.placeholder(tf.float64, [None, z_dim], name='z')
 
 g, g_vars = generator(
     z, width=width, depth=depth, activation=activation, out_dim=out_dim)
+g_read_only, _ = generator(
+    z, width=width, depth=depth, activation=activation, out_dim=out_dim,
+        reuse=True)
 h_out, ae_out, enc_vars, dec_vars = autoencoder(tf.concat([x, g], 0),
     width=width, depth=depth, activation=activation, z_dim=z_dim, reuse=False)
 enc_x, enc_g = tf.split(h_out, [batch_size, gen_num])
@@ -294,26 +314,27 @@ ae_loss = tf.reduce_mean(tf.square(ae_x - x))
 
 # SET UP MMD LOSS.
 mmd, exp_object = compute_mmd(enc_x, enc_g)
-
 d_loss = ae_loss - lambda_mmd * mmd
 g_loss = mmd
 
+lr = tf.Variable(learning_rate, name='lr', trainable=False)
+lr_update = tf.assign(lr, tf.maximum(lr * 0.5, 1e-8), name='lr_update')
 if optimizer == 'adagrad':
-    ae_opt = tf.train.AdagradOptimizer(learning_rate)
-    d_opt = tf.train.AdagradOptimizer(learning_rate)
-    g_opt = tf.train.AdagradOptimizer(learning_rate)
+    ae_opt = tf.train.AdagradOptimizer(lr)
+    d_opt = tf.train.AdagradOptimizer(lr)
+    g_opt = tf.train.AdagradOptimizer(lr)
 elif optimizer == 'adam':
-    ae_opt = tf.train.AdamOptimizer(learning_rate)
-    d_opt = tf.train.AdamOptimizer(learning_rate)
-    g_opt = tf.train.AdamOptimizer(learning_rate)
+    ae_opt = tf.train.AdamOptimizer(lr)
+    d_opt = tf.train.AdamOptimizer(lr)
+    g_opt = tf.train.AdamOptimizer(lr)
 elif optimizer == 'rmsprop':
-    ae_opt = tf.train.RMSPropOptimizer(learning_rate)
-    d_opt = tf.train.RMSPropOptimizer(learning_rate)
-    g_opt = tf.train.RMSPropOptimizer(learning_rate)
+    ae_opt = tf.train.RMSPropOptimizer(lr)
+    d_opt = tf.train.RMSPropOptimizer(lr)
+    g_opt = tf.train.RMSPropOptimizer(lr)
 else:
-    ae_opt = tf.train.GradientDescentOptimizer(learning_rate)
-    d_opt = tf.train.GradientDescentOptimizer(learning_rate)
-    g_opt = tf.train.GradientDescentOptimizer(learning_rate)
+    ae_opt = tf.train.GradientDescentOptimizer(lr)
+    d_opt = tf.train.GradientDescentOptimizer(lr)
+    g_opt = tf.train.GradientDescentOptimizer(lr)
 
 # Define optim nodes.
 # Clip encoder gradients.
@@ -333,9 +354,10 @@ summary_op = tf.summary.merge([
     tf.summary.scalar("loss/d_loss", d_loss),
     tf.summary.scalar("loss/ae_loss", ae_loss),
     tf.summary.scalar("loss/mmd", mmd),
+    tf.summary.scalar("lr/lr", lr),
 ])
 
-# train().
+# main()
 init_op = tf.global_variables_initializer()
 saver = tf.train.Saver()
 summary_writer = tf.summary.FileWriter(checkpoint_dir)
@@ -351,33 +373,91 @@ sv = tf.train.Supervisor(logdir=checkpoint_dir,
                         global_step=step,
                         ready_for_local_init_op=None)
 sess = tf.Session(config=sess_config)
-#global_step = tf.train.get_or_create_global_step()
-#sess = tf.train.MonitoredTrainingSession(
-#    config=sess_config,
-#    checkpoint_dir=checkpoint_dir,
-#    hooks=[tf.train.StepCounterHook])
-
 sess.run(init_op)
+
+# Set save tag, as a function of config parameters.
+save_tag = '{}_dn{}_bs{}_gen{}_zd{}_w{}_d{}_lr{}_op_{}_lbmmd{}'.format(
+    tag, data_num, batch_size, gen_num, z_dim, width, depth, learning_rate,
+    optimizer, lambda_mmd)
+with open(os.path.join(log_dir, 'save_tag.txt'), 'w') as save_tag_file:
+    save_tag_file.write(save_tag)
 print save_tag
+
+# Set up output file.
 g_out_file = os.path.join(log_dir, 'g_out.txt')
 if os.path.isfile(g_out_file):
     os.remove(g_out_file)
+
+# Start time.
 start_time = time()
-pretrain_steps = 0
-for step in range(max_step):
+
+# Load existing model.
+if load_existing:
+    could_load, checkpoint_counter = load_checkpoint(
+        saver, sess, checkpoint_dir)
+    if could_load:
+        load_step = checkpoint_counter
+        print(" [*] Load SUCCESS")
+    else:
+        print(" [!] Load failed...")
+else:
+    load_step = 0
+
+# If only sampling from existing model, do sample and exit before training.
+# sample_n()
+if load_existing and sample_n:
+    random_batch_data = np.array(
+        [data[d] for d in np.random.choice(len(data), batch_size)])
+    random_batch_z = get_random_z(sample_n, z_dim)
+
+    g_ = sess.run(g_read_only,
+        feed_dict={
+            z: random_batch_z})
+            #x: random_batch_data})
+
+    g_out = (g_ * std_vec[cols_to_use] + mean_vec[cols_to_use])
+    for i, row in enumerate(g_out):
+        if row[17] < 0.5:
+            row[17] = 0.0
+        else:
+            row[17] = 1.0
+        if row[18] < 0.5:
+            row[18] = 0.0
+        else:
+            row[18] = 1.0
+        if row[19] < 0.5:
+            row[19] = 0.0
+        else:
+            row[19] = 1.0
+        g_out[i] = row
+    out_name = os.path.join(log_dir, 'g_out_{}'.format(sample_n))
+    np.save(out_name + '.npy', g_out)
+    np.savetxt(out_name + '.csv', g_out, delimiter=',')
+    print('Saved npy and csv of:\n{}'.format(out_name))
+    sys.exit('Finished sampling n.')
+elif load_existing:
+    print('Contining training on checkpoint_dir:\n  {}.'.format(
+        checkpoint_dir))
+else:
+    print('Training new model, and storing at checkpoint_dir:\n  {}.'.format(
+        checkpoint_dir))
+
+
+# train().
+if int(raw_input('\nContinue? [0/1]: ')) != 1:
+    sys.exit()
+for step in range(load_step, max_step):
     random_batch_data = np.array(
         [data[d] for d in np.random.choice(len(data), batch_size)])
     random_batch_z = get_random_z(gen_num, z_dim)
-    if step < pretrain_steps:
-        sess.run([ae_optim],
-                 feed_dict={
-                     z: random_batch_z,
-                     x: random_batch_data})
-    else:
-        sess.run([d_optim, g_optim],
-                 feed_dict={
-                     z: random_batch_z,
-                     x: random_batch_data})
+
+    sess.run([d_optim, g_optim],
+             feed_dict={
+                 z: random_batch_z,
+                 x: random_batch_data})
+
+    if step % lr_update_step == lr_update_step - 1:
+        sess.run(lr_update)
 
     # Occasionally log/plot results.
     if step % log_step == 0:
@@ -390,13 +470,15 @@ for step in range(max_step):
         summary_writer.add_summary(summary_, step)
         summary_writer.flush()
 
-        #enc_x_, enc_g_, exp_object_, mmd_ = sess.run(
-        #    [enc_x, enc_g, exp_object, mmd],
-        #        feed_dict={
-        #            z: random_batch_z,
-        #            x: random_batch_data})
-        #print(np.exp(-0.5 * exp_object_))
-        #pdb.set_trace()
+        '''
+        enc_x_, enc_g_, exp_object_, mmd_ = sess.run(
+            [enc_x, enc_g, exp_object, mmd],
+                feed_dict={
+                    z: random_batch_z,
+                    x: random_batch_data})
+        print(np.exp(-0.5 * exp_object_))
+        pdb.set_trace()
+        '''
 
         # Print some loss values.
         d_loss_, ae_loss_, mmd_, g_ = sess.run(
@@ -407,8 +489,8 @@ for step in range(max_step):
             'L * mmd = {:.4f}, mmd = {:.4f}'.format(step, d_loss_, ae_loss_,
                 lambda_mmd * mmd_, mmd_))
 
-        # Print average of marginal MMD(data_i, gen_i).
         '''
+        # Difference of pairwise MMDs.
         if data_file:
             #num_marginals = len(cols_to_use)
             marginal_mmds = np.zeros((num_cols, num_cols))
@@ -429,8 +511,8 @@ for step in range(max_step):
         '''
 
         # Save generated data to NumPy file and to output collection.
+        # NOTE: First un-norm, then round values in binary cols.
         g_out = (g_ * std_vec[cols_to_use] + mean_vec[cols_to_use])
-        # Round values in binary cols.
         for i, row in enumerate(g_out):
             if row[17] < 0.5:
                 row[17] = 0.0
@@ -440,23 +522,15 @@ for step in range(max_step):
                 row[18] = 0.0
             else:
                 row[18] = 1.0
+            if row[19] < 0.5:
+                row[19] = 0.0
+            else:
+                row[19] = 1.0
             g_out[i] = row
         np.save(os.path.join(log_dir, 'g_out.npy'), g_out)
         np.savetxt(os.path.join(log_dir, 'g_out.csv'), g_out, delimiter=',')
         with open(g_out_file, 'a') as f:
             f.write(str(g_out) + '\n')
-
-        # Print time performance.
-        if step % log_step == 0:
-            elapsed_time = time() - start_time
-            time_per_iter = elapsed_time / step
-            total_est = elapsed_time / step * max_step
-            m, s = divmod(total_est, 60)
-            h, m = divmod(m, 60)
-            total_est_str = '{:.0f}:{:02.0f}:{:02.0f}'.format(h, m, s)
-            print(('  time (s): {:.2f}, time/iter: {:.4f},'
-                   ' Total est.: {}').format(
-                       elapsed_time, time_per_iter, total_est_str))
 
         # PLOTTING RESULTS.
         plot = 1
@@ -488,19 +562,59 @@ for step in range(max_step):
             pairplot_simulation.savefig('pairplot_simulation.png')
             plt.close('all')
             '''
+            # Plot marginals.
             num_cols = raw_data.shape[1]
             sq_dim = int(np.ceil(np.sqrt(num_cols)))
             fig, axs = plt.subplots(4, 5, figsize=(20, 16))
             fig.subplots_adjust(hspace=0.5, wspace=0.5)
             fig.suptitle('Marginals, it{}'.format(step))
             axs = axs.ravel()
+            bins = 40
             for ind_i, i in enumerate(cols_to_use):
                 mmd_i_data_gen, _ = compute_mmd(
                     random_batch_data[:, ind_i], g_[:, ind_i], use_tf=False)
-                axs[i].hist(raw_data[:, ind_i], normed=True, alpha=0.3, label='d')
-                axs[i].hist(g_out[:, ind_i], normed=True, alpha=0.3, label='g')
+                plot_d = raw_data[:, ind_i]
+                plot_g = g_out[:, ind_i]
+                axs[i].hist(plot_d, normed=True, alpha=0.3, label='d', bins=bins)
+                axs[i].hist(plot_g, normed=True, alpha=0.3, label='g', bins=bins)
                 axs[i].set_xlabel('mmd = {:.3f}'.format(mmd_i_data_gen))
                 axs[i].legend()
-            #fig.tight_layout()
-            plt.savefig('plot_marginals.png')
+            plt.savefig(os.path.join(log_dir, 'plot_marginals_{}.png'.format(
+                step)))
             plt.close('all')
+            
+            # Plot correlations.
+            corr_coefs_data = np.zeros((num_cols, num_cols))
+            corr_coefs_gens = np.zeros((num_cols, num_cols))
+            for ind_i, i in enumerate(cols_to_use):
+                for ind_j, j in enumerate(cols_to_use):
+                    if j > i:
+                        corr_coefs_data[i][j], _ = pearsonr(
+                                raw_data[:, ind_i], raw_data[:, ind_j])
+                        corr_coefs_gens[i][j], _ = pearsonr(
+                                g_out[:, ind_i], g_out[:, ind_j])
+            coefs_d = corr_coefs_data.flatten()
+            coefs_g = corr_coefs_gens.flatten()
+            coefs_d = coefs_d[coefs_d != 0]
+            coefs_g = coefs_g[coefs_g != 0]
+            fig, ax = plt.subplots()
+            ax.scatter(coefs_d, coefs_g)
+            ax.plot(ax.get_xlim(), ax.get_ylim(), ls='-')
+            ax.set_xlabel('Correlation data')
+            ax.set_ylabel('Correlation gens')
+            plt.savefig(os.path.join(log_dir, 'plot_correlations_{}.png'.format(
+                step)))
+            plt.close('all')
+
+        # Print time performance.
+        if step % log_step == 0 and step > 0:
+            elapsed_time = time() - start_time
+            time_per_iter = elapsed_time / step
+            total_est = elapsed_time / step * max_step
+            m, s = divmod(total_est, 60)
+            h, m = divmod(m, 60)
+            total_est_str = '{:.0f}:{:02.0f}:{:02.0f}'.format(h, m, s)
+            print(('  time (s): {:.2f}, time/iter: {:.4f},'
+                   ' Total est.: {}').format(
+                       elapsed_time, time_per_iter, total_est_str))
+
